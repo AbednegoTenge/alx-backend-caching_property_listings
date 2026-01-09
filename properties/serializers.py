88020ea48f3cd65712e31071afd610206614b345
Abednego Tenge <abednegotenge180@gmail.com>
2026-01-09 22:15:12 +0000

    setting up cached page for property list view

---
+++ properties/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Property
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ['id', 'title', 'description', 'location', 'price', 'created_at']      
+
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Property
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.views.decorators.cache import cache_page
+from .serializers import PropertySerializer
+from rest_framework import status
 
 # Create your views here.
+
+@cache_page(60 * 15)  # Cache the view for 15 minutes
+@api_view(['GET'])
+def property_list(request):
+    properties = Property.objects.all().order_by('-created_at')
+    serializer = PropertySerializer(properties, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+    
