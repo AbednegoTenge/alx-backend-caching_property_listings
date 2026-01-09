7a7ad7b7451d0874a6c91307b6732e90622345d6
Abednego Tenge <abednegotenge180@gmail.com>
2026-01-09 22:20:55 +0000

    setting up cached page for property list view

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,9 @@
 from .models import Property
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.views.decorators.cache import cache_page
 from .serializers import PropertySerializer
 from rest_framework import status
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -12,5 +12,5 @@ from rest_framework import status
 def property_list(request):
     properties = Property.objects.all().order_by('-created_at')
     serializer = PropertySerializer(properties, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
     
