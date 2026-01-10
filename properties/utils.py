e58154a68395b0de7c8495183b02450c8f3bd452
Abednego Tenge <abednegotenge180@gmail.com>
2026-01-10 09:20:00 +0000

    setting signals to invalidate cache on create/update/delete

--- properties/utils.py
+++ properties/utils.py
@@ -54,10 +54,7 @@ def get_redis_cache_metrics():
         total_requests = hits + misses
         
         # Calculate hit ratio
-        if total_requests > 0:
-            hit_ratio = hits / total_requests
-        else:
-            hit_ratio = 0
+        hit_ratio = hits / total_requests if total_requests > 0 else 0
 
         # Prepare metrics dictionary
         metrics = {
